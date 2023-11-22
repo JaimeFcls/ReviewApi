9b54b0dbce4b2658b175ec0368d131453b5767db
Jaime <jfcls@discente.ifpe.edu.br>
2023-11-22 20:05:16 -0300

    Listar categorias filmes

--- src/components/SideBarFilter.jsx
+++ src/components/SideBarFilter.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./SideBarFilter.css";
 
 const SideBarFilter = () => {
-    const [filterText, setFilterText] = useState("");
+    const [categories, setCategories] = useState([]);
 
-    /*const handleFilterTextChange = (e) => {
-        setFilterText(e.target.value);
-    };
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTFjM2JmZTU5NmZjMmJiMmQ1OWQwZDhiYWZlMTM2NyIsInN1YiI6IjY0ZGVhYjcyYjc3ZDRiMTEzZmM2MDVhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BwanTcyFlIRs3zxrfDXVXOCt6Cj2bH9AZSyUsNQgAv8'
+            }
+        };
 
-    const handleFilterClick = () => {
-        // Coloque sua lógica de filtro aqui
-        console.log("Filtrar por: " + filterText);
-    };*/
-    
+        fetch('https://api.themoviedb.org/3/genre/movie/list?language=pt-BR', options)
+            .then(response => response.json())
+            .then(response => setCategories(response.genres))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <div className="sidebar-filter">
-            <h3>Filtro</h3>
-            <a href="#">Ação</a>
-            <a href="#">Aventura</a>
-            <a href="#">Animação</a>
-            <a href="#">Comédia</a>
-            <a href="#">Crime</a>
-            <a href="#">Documentário</a>
-            <a href="#">Drama</a>
-            <a href="#">Família</a>
-            <a href="#">Infantil</a>
-
+            <h3>Categorias</h3>
+            {categories.map(category => (
+                <div key={category.id}>
+                    <a href={`/category/${category.id}`}>{category.name}</a>
+                </div>
+            ))}
         </div>
     );
 };
