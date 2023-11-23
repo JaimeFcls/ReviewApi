f0f4315dc2197c789ee1b86bb46be33e24be2866
Jaime <jfcls@discente.ifpe.edu.br>
2023-11-22 21:11:33 -0300

    upt

--- src/components/SideBarFilter.jsx
+++ src/components/SideBarFilter.jsx
@@ -2,39 +2,35 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./SideBarFilter.css";
 
-const categoriaFilmesUrl = import.meta.env.VITE_CATEGORIA;
-
 const SideBarFilter = () => {
-    
-  
-    const getCategoriaFilmes = async (url) => {
-    const res = await fetch(url, {
-        headers : {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTFjM2JmZTU5NmZjMmJiMmQ1OWQwZDhiYWZlMTM2NyIsInN1YiI6IjY0ZGVhYjcyYjc3ZDRiMTEzZmM2MDVhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BwanTcyFlIRs3zxrfDXVXOCt6Cj2bH9AZSyUsNQgAv8',
-        },
-    })
-    useEffect(() => {
-        const categoriaFilmeUrl = `${categoriaFilmesUrl}`
-        getCategoriaFilmes(categoriaFilmeUrl);
-        
-    }
-        ,console.log(getCategoriaFilmes)
-)
+    const [categories, setCategories] = useState([]);
 
-    };
-    
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTFjM2JmZTU5NmZjMmJiMmQ1OWQwZDhiYWZlMTM2NyIsInN1YiI6IjY0ZGVhYjcyYjc3ZDRiMTEzZmM2MDVhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BwanTcyFlIRs3zxrfDXVXOCt6Cj2bH9AZSyUsNQgAv8'
+            }
+        };
+
+        fetch('https://api.themoviedb.org/3/genre/movie/list?language=pt-BR', options)
+            .then(response => response.json())
+            .then(response => setCategories(response.genres))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <div className="sidebar-filter">
             <h3>Categorias Filmes:</h3>
             {categories.map(category => (
-                <div key={category.id}>
-                    <Link to={`/category/${category.id}`}>{category.name}</Link>
+                <div className="categories" key={category.id}>
+                    <Link to={`/category/${category.id}`} style={{ textDecoration: 'none' }}> {category.name } </Link>
                 </div>
             ))}
         </div>
-        
     );
 };
 
 export default SideBarFilter;
+
