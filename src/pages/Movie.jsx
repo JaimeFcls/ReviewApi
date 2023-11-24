b50d99127dda20f73deee34c30a8f51c28f7734c
JaimeFcls <119618310+JaimeFcls@users.noreply.github.com>
2023-11-24 16:48:04 -0300

    att

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -10,30 +10,39 @@ const Movie = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
   const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState("");
+  const [charCount, setCharCount] = useState(0);
   const user = getUser();
+
+  const handleCommentChange = (event) => {
+    const text = event.target.value;
+    if (text.length <= 1500) {
+      setCommentText(text);
+      setCharCount(text.length);
+    }
+  };
 
   const handleCommentSubmit = async (event) => {
     event.preventDefault();
-    const comment = event.target.elements.comment.value;
     const response = await fetch('http://localhost:8082/api/comentar', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        comentar: comment,
-        usuarioId: user.id, 
-        movieId: id, 
-        
+        comentar: commentText,
+        usuarioId: user.id,
+        movieId: id,
       }),
     });
     if (response.ok) {
       const data = await response.json();
       setComments([...comments, data]);
+      setCommentText(""); // Limpa o campo de comentário após o envio
+      setCharCount(0); // Reinicia o contador de caracteres
     } else {
       console.error('Erro ao enviar comentário:', response.statusText);
     }
-    event.target.reset();
   };
 
   const getMovie = async (url) => {
@@ -59,6 +68,7 @@ const Movie = () => {
     const movieUrl = `${moviesURL}${id}?language=pt-br`;
     getMovie(movieUrl);
   }, []);
+
   return (
     <div className="movie-back">
       <img src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`} alt="movie backdrop" style={{ width: '1920px', height: '400px', opacity: "20%" }} />
@@ -68,27 +78,39 @@ const Movie = () => {
           <p className="movie-title">{movie.title}</p>
           <p className="tagline">{movie.tagline}</p>
           <div className="sinopse">
-            <h3>
-              Sinopse
-            </h3>
+            <h3>Sinopse</h3>
             <p>{movie.overview}</p>
-            <br></br>
-            <p>Lançado em : {new Date(movie.release_date).toLocaleDateString('pt-BR')}</p>
+            <br />
+            <p>Lançado em: {new Date(movie.release_date).toLocaleDateString('pt-BR')}</p>
           </div>
           <div className="comments">
-          <h3>Comentar</h3>
-          <br></br>
+            <h3>Comentar</h3>
+            <br />
             {user ? (
               <form onSubmit={handleCommentSubmit}>
-                <textarea className="comentario" name="comment" type="text" placeholder="Adicione um comentário..." required />
+                <textarea
+                  className="comentario"
+                  name="comment"
+                  type="text"
+                  placeholder="Adicione um comentário..."
+                  value={commentText}
+                  onChange={handleCommentChange}
+                  required
+                />
+                <div className="char-count">Caracteres restantes: {1500 - charCount}</div>
                 <button className="comentar" type="submit">Enviar</button>
               </form>
             ) : (
-                <p>Você precisa estar logado para comentar.  <a className="clique" href="/login">Clique aqui para entrar</a></p>
-              
+              <p>Você precisa estar logado para comentar. <a className="clique" href="/login">Clique aqui para entrar</a></p>
             )}
+            <div>
+              <p className="cmt">Comentarios :</p>
+            </div>
             {comments.map((comment, index) => (
-              <p className="falaai" key={index}>{comment.comentar} - {comment.usuario ? comment.usuario.nome : 'Usuário desconhecido'} </p>
+              <div className="ComentarioFinal">
+                <p className="falaai" key={index}>{comment.comentar}</p>
+                <p className="nomeComment"> - {comment.usuario.nome}</p>
+              </div>
             ))}
           </div>
         </>
