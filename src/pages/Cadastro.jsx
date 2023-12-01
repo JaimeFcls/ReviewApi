43263dff2da26579af2229010b55e4d9914f7ef0
Jaime <jfcls@discente.ifpe.edu.br>
2023-12-01 09:32:09 -0300

    .

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -3,7 +3,45 @@ import React, { useState } from "react";
 import "./Cadastro.css";
 
 export default function Cadastro() {
- 
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmarEmail, setConfirmarEmail] = useState(""); 
+  const [senha, setSenha] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [formError, setFormError] = useState("");
+
+  function validarSenha(senha) {
+    if (senha.length < 6) {
+      alert("A senha deve ter no mínimo 6 caracteres.");
+      return false;
+    }
+    return true;
+  }
+
+  function salvar() {
+    if (nome && email === confirmarEmail && validarSenha(senha)) {
+      let usuarioRequest = {
+        nome: nome,
+        senha: senha,
+        email: email,
+      };
+
+      axios.post("http://localhost:8082/api/usuario", usuarioRequest)
+        .then((response) => {
+          alert('Cadastrado com sucesso.');
+          window.location.href = "/login"
+        })
+        .catch((error) => {
+          alert('Email já em utilização');
+        });
+    } else {
+      if (email !== confirmarEmail) {
+        alert("Verifique o email digitado.");
+      } else {
+        alert("Por favor, preencha todos os campos corretamente.");
+      }
+    }
+  }
 
   return (
     <section>
@@ -16,7 +54,7 @@ export default function Cadastro() {
                 type="text"
                 placeholder="Nome"
                 required
-                
+                onChange={e => setNome(e.target.value)}
               />
               <label>Nome</label>
             </div>
@@ -25,7 +63,7 @@ export default function Cadastro() {
                 type="email"
                 placeholder="Email"
                 required
-              
+                onChange={e => setEmail(e.target.value)}
               />
               <label>Email</label>
             </div>
@@ -34,7 +72,7 @@ export default function Cadastro() {
                 type="email"
                 placeholder="Confirmar Email"
                 required
-               
+                onChange={e => setConfirmarEmail(e.target.value)} 
               />
               <label>Confirmar Email</label>
             </div>
@@ -43,13 +81,16 @@ export default function Cadastro() {
                 type="password"
                 placeholder="Senha"
                 required
-               
+                onChange={e => {
+                  setSenha(e.target.value);
+                  setPasswordError(""); 
+                }}
               />
               <label>Senha</label>
             </div>
             
 
-            <button type="button">Cadastrar</button>
+            <button type="button" onClick={salvar}>Cadastrar</button>
             <div className="register">
               <p>Já tem uma conta? <a href="/login">Login</a></p>
             </div>
