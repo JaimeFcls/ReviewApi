032a04d27a1347bf5bd4c84ffb0ea8dcb1a94a03
Jaime <jfcls@discente.ifpe.edu.br>
2023-12-19 10:02:36 -0300

    upt

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -86,7 +86,12 @@ const Series = () => {
     };
     const handleCommentDelete = async (commentId) => {
         if (window.confirm('Tem certeza de que deseja excluir este comentário? As respostas obtidas também serão excluidas')){
-        const response = await fetch(`https://xt4a713djcwo.share.zrok.io/api/comentar/${commentId}`, {
+            const repliesToDelete = replies.filter(reply => reply.comentario?.id === commentId);
+            for (let reply of repliesToDelete) {
+                await handleReplyDelete(reply.id);
+            }
+        
+            const response = await fetch(`https://xt4a713djcwo.share.zrok.io/api/comentar/${commentId}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
